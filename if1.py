b0effc8d87cbb9ae308e7f680b2f61bdde81667f
Natalya Komarova <shadowx110@yandex.ru>
2020-02-18 23:31:02 +0300

    Добавил файл с дз к блоку for (программа работает без использования шаблона

--- if1.py
+++ if1.py
@@ -13,13 +13,49 @@
 * Вывести содержимое переменной на экран
 
 """
+age_user = input('Пожалуйста введите свой возраст:\n')
 
-def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
+def selection(age):
+    if age != '':
+        age = int(age)
+        if 0 <= age <=2:
+            return 'Вам еще рано в сад. Сидите дома с мамой!'
+        elif 3 <= age < 7:
+            return 'Вы посещаете дошкольное учебное заведение :)'
+        elif 7 <= age <= 17:
+            return 'Да вы уже школьник XD'
+        elif 18 <= age <= 23:
+            return 'Студент, не прогуливайте лекции!'
+        else:
+            return "Если вы еще не работаете, то пора начинать!"
+    else:
+        return 'Неверный формат ввода'
 
-if __name__ == "__main__":
-    main()
+
+def format_input(input_user):
+    input_user = input_user.split(' ')
+    input_user =''.join([str(elem) for elem in input_user])
+    if input_user.isalpha() == False:
+        if input_user.isdigit() == True:
+            age_int = selection(input_user) 
+            return age_int
+        else:
+            input_user = float(input_user)
+            age_float = selection(input_user)
+            return age_float
+    else:
+        return 'Введите число цифрами'
+
+your_activity = format_input(age_user)
+print(your_activity)
+
+
+# def main():
+#     """
+#     Эта функция вызывается автоматически при запуске скрипта в консоли
+#     В ней надо заменить pass на ваш код
+#     """
+#     pass
+
+# if __name__ == "__main__":
+#     main()
