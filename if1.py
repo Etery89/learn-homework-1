cfab70e3827f6daa15abd6c022472cd21d587731
unknown <User@Vinogradova.niias.ru>
2020-02-18 13:28:41 +0300

    Модифицировала файлы if1.py и if2.py(пометка: программы работают самостоятельно, в шаблоне нет)

--- if1.py
+++ if1.py
@@ -13,13 +13,66 @@
 * Вывести содержимое переменной на экран
 
 """
+age_user = input('Пожалуйста введите свой возраст:\n')
 
-def main():
+def main(age):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    if age != '':
+        age = int(age)
+        if 0 <= age <=2:
+            return 'Вам еще рано в сад. Сидите дома с мамой!'
+        elif 3 <= age < 7:
+            return 'Вы посещаете дошкольное учебное заведение :)'
+        elif 7 <= age <= 17:
+            return 'Да вы уже школьник XD'
+        elif 18 <= age <= 23:
+            return 'Студент, не прогуливайте лекции!'
+        else:
+            return "Если вы еще не работаете, то пора начинать!"
+    else:
+        return 'Неверный формат ввода'
 
 if __name__ == "__main__":
-    main()
+    main(age_user)
+
+# # Вот моя программа. Отдельно от скрипта она работает. 
+# Однако когда я помещаю внутрь шаблона 
+# даже просто функцию определения активности, то ничего не работает.(см выше)
+# age_user = input('Пожалуйста введите свой возраст:\n')
+
+# def selection(age):
+#     if age != '':
+#         age = int(age)
+#         if 0 <= age <=2:
+#             return 'Вам еще рано в сад. Сидите дома с мамой!'
+#         elif 3 <= age < 7:
+#             return 'Вы посещаете дошкольное учебное заведение :)'
+#         elif 7 <= age <= 17:
+#             return 'Да вы уже школьник XD'
+#         elif 18 <= age <= 23:
+#             return 'Студент, не прогуливайте лекции!'
+#         else:
+#             return "Если вы еще не работаете, то пора начинать!"
+#     else:
+#         return 'Неверный формат ввода'
+
+
+# def format_input(input_user):
+#     input_user = input_user.split(' ')
+#     input_user =''.join([str(elem) for elem in input_user])
+#     if input_user.isalpha() == False:
+#         if input_user.isdigit() == True:
+#             age_int = selection(input_user) 
+#             return age_int
+#         else:
+#             input_user = float(input_user)
+#             age_float = selection(input_user)
+#             return age_float
+#     else:
+#         return 'Введите число цифрами'
+
+# your_activity = format_input(age_user)
+# print(your_activity)
--- if2.py
+++ if2.py
@@ -14,13 +14,61 @@
   и выводя на экран результаты
 
 """
+str1 = 7906
+str2 = 'learn' 
 
-def main():
+def main(str_1, str_2):
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+  if isinstance(str_1, str) == True and isinstance(str_2, str) == True:
+    if str_1 == str_2:
+        return 1
+    elif str_1 != str_2 and len(str_1) > len(str_2):
+        return 2
+    elif str_1 != str_2 and str_2 == 'learn':
+        return 3
+    else:
+        return 4
+  else:
+    return 0  
     
 if __name__ == "__main__":
-    main()
+    main(str1, str2)
+
+# Текст моей программы. При помещении в шаблон ничего не работает. 
+# Без шаблона всё ок 
+# str1 = 7906
+# str2 = 'learn'
+# str3 = 'Congratulations'
+# str4 = 'Python'
+# str5 = 'Go'
+# str6 = 'learn'
+# str7 = 'Hello'
+# str8 = 'Natalya Komarova'
+
+
+
+
+# def str_comparison(str_1, str_2):
+#     if isinstance(str_1, str) == True and isinstance(str_2, str) == True:
+#         if str_1 == str_2:
+#             return 1
+#         elif str_1 != str_2 and len(str_1) > len(str_2):
+#             return 2
+#         elif str_1 != str_2 and str_2 == 'learn':
+#             return 3
+#         else:
+#             return 4
+#     else:
+#         return 0    
+
+# comparison_1 = str_comparison(str1, str2)
+# print(comparison_1)
+# comparison_2 = str_comparison(str3, str4)
+# print(comparison_2)
+# comparison_3 = str_comparison(str5, str6)
+# print(comparison_3)
+# comparison_4 = str_comparison(str7, str8)
+# print(comparison_4)
