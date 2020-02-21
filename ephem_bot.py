7acbce3b040d2baf1b9c62359dcf601bab912e8c
Natalya Komarova <shadowx110@yandex.ru>
2020-02-21 19:02:42 +0300

    Добавила дз к блоку про модули

--- ephem_bot.py
+++ ephem_bot.py
@@ -12,6 +12,7 @@
   бота отвечать, в каком созвездии сегодня находится планета.
 
 """
+import ephem
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -20,7 +21,6 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log'
 )
-
 
 PROXY = {
     'proxy_url': 'socks5://t1.learn.python.ru:1080',
@@ -36,6 +36,15 @@ def greet_user(bot, update):
     print(text)
     update.message.reply_text(text)
 
+def planet_name_show(bot, update):
+  user_write = 'planet Mars'
+  user_planet = user_write.split(' ')
+  name_planet = user_planet[1]
+  update.message.reply_text(name_planet)
+  if name_planet == 'Mars':
+    tmp = ephem.Mars('2020/02/21')
+    const_for_planet = ephem.constellation(tmp)
+    update.message.reply_text(const_for_planet)
 
 def talk_to_me(bot, update):
     user_text = update.message.text 
@@ -44,10 +53,11 @@ def talk_to_me(bot, update):
  
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY)
+    mybot = Updater("947602389:AAFnfgNJB9Aob1HNBSko0qiEKgGvhaVVmkM", request_kwargs=PROXY)
     
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(CommandHandler("planet", planet_name_show))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     mybot.start_polling()
